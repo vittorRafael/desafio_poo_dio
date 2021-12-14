2a40fe0de63ca3af25b3c50da6d24254940f501a
vittorRafael <2017infor45@gmail.com>
2021-12-14 09:58:18 -0300

    Criando a classe abstrata conteudo para org curso e mentoria.

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,27 +1,14 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String desc;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHoraria() {
@@ -35,8 +22,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", desc='" + desc + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", desc='" + getDesc() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,28 +2,15 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String desc;
+public class Mentoria extends Conteudo {
     private LocalDate data;
 
     public Mentoria() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
     }
 
     public LocalDate getData() {
@@ -37,8 +24,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", desc='" + desc + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", desc='" + getDesc() + '\'' +
                 ", data=" + data +
                 '}';
     }
